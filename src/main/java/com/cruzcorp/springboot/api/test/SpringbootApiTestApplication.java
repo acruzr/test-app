3d8f2dda767bc6b646afea8618af4ee85f4ba853
acruzr <acruzr135@gmail.com>
2020-08-26 16:36:50 -0500

    initial scaffolding project

---
+++ src/main/java/com/cruzcorp/springboot/api/test/SpringbootApiTestApplication.java
@@ -0,0 +1,15 @@
+package com.cruzcorp.springboot.api.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringbootApiTestApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringbootApiTestApplication.class, args);
+    }
+
+}
